126799fad07d797f6239bd15efcad2e0497e8770
syf <2654525303@qq.com>
2023-02-16 22:17:38 +0800

    add List2D.cs
    - add List2DTests.cs for unit testing

---
+++ SnekPluginTest/GridTests.cs
@@ -0,0 +1,9 @@
+﻿namespace SnekPluginTest;
+
+public class GridTests
+{
+    [Test]
+    public void grid_can_be_initiated()
+    {
+    }
+}
---
+++ SnekPluginTest/List2DTests.cs
@@ -0,0 +1,53 @@
+﻿using FluentAssertions;
+using SnekPlugin.MineSweeper;
+
+namespace SnekPluginTest;
+
+public class List2DTests
+{
+    private readonly List2D<int> _list2D = new List2D<int>();
+
+    private static readonly int[,] arr1 = {
+        {1, 2, 3},
+        {4, 5, 6},
+    };
+
+    [SetUp]
+    public void Setup()
+    {
+        _list2D.ResetWith(arr1);
+    }
+
+    [TestCase(0, 0, 1)]
+    [TestCase(1, 1, 5)]
+    public void can_Retrieve_AtIndex(int rowIndex, int columnIndex, int value)
+    {
+        _list2D.ResetWith(arr1);
+
+        var result = _list2D[rowIndex][columnIndex];
+
+        result.Should().Be(value);
+    }
+
+    [Test]
+    public void is_enumerable()
+    {
+        _list2D.ResetWith(arr1);
+
+        var count = _list2D.Count();
+
+        count.Should().Be(6);
+    }
+
+    [Test]
+    public void can_init_with_2d_array()
+    {
+        var list2D = new List2D<int>(new[,]
+        {
+            {1, 2, 3},
+            {4, 5, 6},
+        });
+
+        list2D[0][2].Should().Be(3);
+    }
+}
