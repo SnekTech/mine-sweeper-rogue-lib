3fa66a8ffa0d4fb7b328c3faddbc80dd1a592986
syf <2654525303@qq.com>
2023-02-18 00:38:21 +0800

    add FA formatters

---
+++ SnekPluginTest/Formatters/GridIndexValueFormatter.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions.Formatting;
+using SnekPlugin.MineSweeper.Grid;
+
+namespace SnekPluginTest.Formatters;
+
+public class GridIndexValueFormatter : IValueFormatter
+{
+    public bool CanHandle(object value)
+    {
+        return value is GridIndex;
+    }
+
+    public void Format(object value, FormattedObjectGraph formattedGraph, FormattingContext context, FormatChild formatChild)
+    {
+        var gridIndex = (GridIndex) value;
+        var stringToRender = $"{nameof(gridIndex)}(i: {gridIndex.RowIndex}, j: {gridIndex.ColumnIndex})";
+
+        if (context.UseLineBreaks)
+        {
+            formattedGraph.AddLine(stringToRender);
+        }
+        else
+        {
+            formattedGraph.AddFragment(stringToRender);
+        }
+    }
+}
---
+++ SnekPluginTest/Initializer.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+using SnekPluginTest.Formatters;
+
+namespace SnekPluginTest;
+
+public static class Initializer
+{
+    [ModuleInitializer]
+    public static void SetDefaults()
+    {
+        FluentAssertions.Formatting.Formatter.AddFormatter(new GridIndexValueFormatter());
+        FluentAssertions.Formatting.Formatter.AddFormatter(new GridSizeValueFormatter());
+    }
+}
