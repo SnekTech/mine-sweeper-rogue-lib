5cced63bf43721302013fe6b97f43cc4a2b1f792
syf <2654525303@qq.com>
2023-02-18 01:47:58 +0800

    test cell parent accessing

---
+++ SnekPluginTest/MineSweeper/Tests/Cell/CellBasicTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using SnekPlugin.MineSweeper.Grid;
+using SnekPluginTest.MineSweeper.Builders;
+
+namespace SnekPluginTest.MineSweeper.Tests;
+
+public class CellBasicTests
+{
+    private static readonly IHumbleGrid SimpleHumbleGrid = A.MockHumbleGridBuilder.Build();
+
+    [Test]
+    public void can_access_parent()
+    {
+        // Arrange
+        BombMatrix bombMatrix = A.BombMatrix
+            .WithArray2D(new[,] {{1}});
+        Grid grid = A.Grid
+            .WithBombMatrix(bombMatrix)
+            .WithHumbleGrid(SimpleHumbleGrid);
+
+        // Act
+        var cell = grid.GetCellAt(new GridIndex(0, 0));
+
+        // Assert
+        cell.Parent.Should().BeSameAs(grid);
+    }
+}
